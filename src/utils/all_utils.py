6b2435b3ea3dca43b636abcc221261c75712d623
Surreal Gyan <gyannetics@gmail.com>
2021-09-26 09:51:03 +0530

    added dvc, config files

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -10,4 +10,4 @@ def read_yaml(path_to_file) -> dict:
 def create_dir(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
-        print(f"directorey created at {dir_path}")
+        print(f"directory created at {dir_path}")
