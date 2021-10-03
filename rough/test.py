bb422f63396489288259722f83824e3f7da71e52
Surreal Gyan <gyannetics@gmail.com>
2021-10-03 09:15:17 +0530

    stage 02 added

---
+++ rough/test.py
@@ -0,0 +1,6 @@
+def sanity_check():
+    pass
+
+
+if __name__=='__main__':
+    sanity_check()
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 
-def read_yaml(path_to_file) -> dict:
+def read_yaml(path_to_file: str) -> dict:
     with open(path_to_file) as yaml_file:
         content = yaml.safe_load(yaml_file)
 
@@ -11,3 +11,7 @@ def create_dir(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
         print(f"directory created at {dir_path}")
+
+def save_local_df(data, data_path, index_state=False):
+    data.to_csv(data_path, index=index_state)
+    print(f"data saved !!")
